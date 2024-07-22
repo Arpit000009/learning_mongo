89b6b5297487f7ddf299a6e1bc129cf31e42d6db
Arpit000009 <arpitpandey000009@gmail.com>
2024-07-22 13:47:20 +0530

    users data

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express')
-const app = express()
+const app = express();
+const path =require('path')
+const userModel = require('./models/user')
 
-const userModel = require('./usermodel')
 
-app.get('/',(req,res)=>{
-    res.send("hey")
+app.set("view engine","ejs")
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res) => {
+    res.render("index");
 })
 
-app.get('/create',async(req,res)=>{
-    let createduser = await userModel.create({
-    name: "arpit",
-    email: "arpitpandey@gmail.com",
-    username:"arpit9"
-   })
-   res.send(createduser)
+app.get('/read',async (req,res)=>{
+    let allusers=await userModel.find()
+    res.render("read",{users:allusers});
+})
+app.get('/delete/:id',async (req,res)=>{
+    let allusers =await userModel.findOneAndDelete({_id: req.params.id})
+    res.redirect("/read");
 })
 
-app.listen(3000)
+app.post('/create',async (req,res)=>{
+    let{name, email, image} = req.body;
+
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.redirect('/read')
+          
+})
+
+app.listen(3000);
+
+
+
